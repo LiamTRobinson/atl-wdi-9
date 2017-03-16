d405a16f44eee8e9151d94f36151d62853bc8c25
LiamTRobinson <Liamtrobinson@gmail.com>
2017-03-16 15:04:05 -0400

    pirates done

--- unit_02/w06d04/homework/pirates-wiki/controllers/pirates.js
+++ unit_02/w06d04/homework/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,31 @@
-//your code below
+//packages
+var express = require("express");
+var router = express.Router();
+var data = require("../models/pirates.js");
+
+//paths
+router.get("/", function(req, res){
+	res.render("pirates/index", {
+		data: data
+	});
+});
+
+router.get("/new", function(req, res) {
+	res.render("pirates/new", {
+		
+	});
+});
+
+router.get("/:id", function(req, res){
+	res.render("pirates/show", {
+		pirate: data[req.params.id]
+	});
+});
+
+router.post("/", function(req, res) {
+	data.push(req.body);
+	res.redirect("/pirates");
+});
+
+
+module.exports = router;
--- unit_02/w06d04/homework/pirates-wiki/server.js
+++ unit_02/w06d04/homework/pirates-wiki/server.js
@@ -1,1 +1,24 @@
-//your code here
+//packages
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+var hbs = require("hbs");
+app.set("view engine", "hbs");
+
+//uses
+app.use(express.static(__dirname + '/public'));
+
+
+//paths
+app.get("/", function(req, res) {
+	res.send("This is the home page!")
+});
+
+var piratesController = require("./controllers/pirates.js");
+app.use("/pirates", piratesController);
+
+
+app.listen(3000, function(){
+	console.log("listening");
+});
